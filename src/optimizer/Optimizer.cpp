6a0530d606d89ed953671147366e813921277844
Ruthvik Gajjala <ruthvikgajjala14@gmail.com>
2021-11-30 18:43:52 -0500

    adding command line optimization disabler

--- src/optimizer/Optimizer.cpp
+++ src/optimizer/Optimizer.cpp
@@ -8,24 +8,40 @@
 
 using namespace llvm;
 
-void Optimizer::optimize(Module* theModule) {
+void Optimizer::optimize(Module* theModule, std::string disableOpt) {
   // Create a pass manager to simplify generated module
   auto TheFPM = std::make_unique<legacy::FunctionPassManager>(theModule);
 
-  // Promote allocas to registers.
-  TheFPM->add(createPromoteMemoryToRegisterPass());
+  if (!(disableOpt=="cpmtrp")){
+    // Promote allocas to registers.
+    TheFPM->add(createPromoteMemoryToRegisterPass());
+  }
 
-  // Do simple "peephole" optimizations
-  TheFPM->add(createInstructionCombiningPass());
+  if (!(disableOpt=="cicp")){
+    // Do simple "peephole" optimizations
+    TheFPM->add(createInstructionCombiningPass());
+  }
 
-  // Reassociate expressions.
-  TheFPM->add(createReassociatePass());
+  if (!(disableOpt=="crp")){
+    // Reassociate expressions.
+    TheFPM->add(createReassociatePass());
+  }
 
-  // Eliminate Common SubExpressions.
-  TheFPM->add(createGVNPass());
+  if (!(disableOpt=="cgp")){
+    // Eliminate Common SubExpressions.
+    TheFPM->add(createGVNPass());
+  }
 
-  // Simplify the control flow graph (deleting unreachable blocks, etc).
-  TheFPM->add(createCFGSimplificationPass());
+  if (!(disableOpt=="ccsp")){
+    // Simplify the control flow graph (deleting unreachable blocks, etc).
+    TheFPM->add(createCFGSimplificationPass());
+  }
+
+  if (!(disableOpt=="clup")){
+    // Simple loop unrolling
+    TheFPM->add(createLoopUnrollPass());
+  }
+
 
   // initialize and run simplification pass on each function
   TheFPM->doInitialization();
